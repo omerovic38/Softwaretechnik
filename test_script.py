5b8165dc57adaf75478749c812be08c2bd5094d8
Mehyar Mousa <Mehyar.Mousa@outlook.de>
2020-11-05 15:42:49 +0100

    test_script

---
+++ test_script.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+
+sum = 0
+for i in range(10):
+    sum += 1
+    
+    
+#anderung im Code
+sum = 1
+for i in range(10):
+    sum += 1
+    
+    
